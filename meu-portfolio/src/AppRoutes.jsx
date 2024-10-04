0a6380d079fc4a19f59a77941a8d926851977c55
ViniciusCastroo <vi230206@gmail.com>
2024-10-04 15:51:12 -0300

    A

--- meu-portfolio/src/AppRoutes.jsx
+++ meu-portfolio/src/AppRoutes.jsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
-import Sobre from './pages/Sobre';
-import Projetos from './pages/Projetos';
-import Contato from './pages/Contato';
 
-const AppRoutes = () => {
+
+function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} /> {/* Renderiza Home */}
-          <Route path="/sobre" element={<Sobre />} />
-          <Route path="/projetos" element={<Projetos />} />
-          <Route path="/contato" element={<Contato />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} /> {/* Home será renderizado no Outlet */}
+     
+      </Route>
+    </Routes>
   );
-};
+}
 
 export default AppRoutes;
