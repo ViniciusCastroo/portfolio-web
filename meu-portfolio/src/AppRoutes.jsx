bb6f27c45d79bbdb9b26f91ba593a8f7d3a35fc8
ViniciusCastroo <vi230206@gmail.com>
2024-10-05 16:42:08 -0300

    add img projetos

--- meu-portfolio/src/AppRoutes.jsx
+++ meu-portfolio/src/AppRoutes.jsx
@@ -3,18 +3,18 @@ import App from './App';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Projetos from './pages/Projetos';
-import Contato from './pages/Contato'
-
+import Contato from './pages/Contato';
+import ProjetoDetalhes from './components/ProjetoDetalhes';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<App />}>
-        <Route index element={<Home />} /> {/* Home será renderizado no Outlet */}
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/projetos" element={<Projetos />} />
-        <Route path="/contato" element={<Contato />} />
-     
+        <Route index element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="projetos" element={<Projetos />} />
+        <Route path="projetos/:id" element={<ProjetoDetalhes />} />
+        <Route path="contato" element={<Contato />} />
       </Route>
     </Routes>
   );
